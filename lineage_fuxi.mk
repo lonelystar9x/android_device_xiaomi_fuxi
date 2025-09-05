e5f77d296e2755a708ab75be52d7b228c6353d95
Quang Minh <38067026+lonelystar9x@users.noreply.github.com>
2025-09-05 19:50:48 +0700

    Add flags for Rising16

--- lineage_fuxi.mk
+++ lineage_fuxi.mk
@@ -14,10 +14,31 @@ $(call inherit-product, device/xiaomi/fuxi/device.mk)
 # Inherit LineageOS configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Camera
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+
+# Rom flags
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_PREBUILT_BCR := false
+TARGET_INCLUDE_ACCORD := false
+TARGET_DISABLE_EPPE := true
+WITH_GMS := true
+TARGET_USES_PICO_GAPPS := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+
+# Device identifier
 PRODUCT_DEVICE := fuxi
 PRODUCT_NAME := lineage_fuxi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2211133C
 PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon® 8 Gen 2" \
+    RisingMaintainer="™lonelystar™"
 
 BUILD_FINGERPRINT := Xiaomi/fuxi/fuxi:15/AQ3A.240912.001/OS2.0.213.0.VMCCNXM:user/release-keys
